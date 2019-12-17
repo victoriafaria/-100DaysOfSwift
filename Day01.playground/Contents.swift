1fcc56dcecf3459499b49180a6b7033d1e379e3a
Victoria Andressa Faria <victoriaamdf@gmail.com>
2019-12-17 02:36:22 -0300

    Day 01
    Variables
        Strings and integers
        Multi-line strings
        Doubles and booleans
        String interpolation
        Constants
        Type annotations
        Simple types: Summary

---
+++ Day01.playground/Contents.swift
@@ -0,0 +1,37 @@
+/*
+    Variables
+    Strings and integers
+    Multi-line strings
+    Doubles and booleans
+    String interpolation
+    Constants
+    Type annotations
+    Simple types: Summary
+ 
+    by @VickAndressaDev
+
+*/
+
+import UIKit
+
+
+var name = "Victoria"
+var age = 21
+var money = 12.90
+var isRight = true
+
+var textBlock = """
+Here is a block \
+of text that is divided \
+in more than one line
+"""
+
+print(textBlock)
+
+print("My name is \(name), I'm \(age) years old. Have I only R$\(money) Reais? Yeah, this information is \(isRight)")
+
+// not change
+let Button = "pressed"
+
+
+
