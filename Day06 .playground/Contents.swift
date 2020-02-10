7843ff16f7d3e63255e2a8542a52e4eeae222583
Victoria Andressa Faria <victoriaamdf@gmail.com>
2020-02-09 23:41:51 -0300

    update day 06 with closure

--- Day06 .playground/Contents.swift
+++ Day06 .playground/Contents.swift
@@ -37,3 +37,28 @@ let message =  drivingWithReturn("Curitiba")
 print(message)
 
 
+let bakeBirthdayCake = { (name: String) -> Int in
+    print("I've made a cake for \(name); here's the bill.")
+    return 50
+}
+print(bakeBirthdayCake)
+
+// Closures as parameters
+func travel(action: () ->  Void ) {
+    print("Eu estou pronto pra ir")
+    action()  //eu estou dirigiindo meu carro
+    print("Vamos, vamos")
+}
+
+travel(action: driving)
+
+let awesomeTalk = {
+    print("Here's a great talk!")
+}
+func deliverTalk(name: String, type: () -> Void) {
+    print("My talk is called \(name)")
+    type()
+}
+deliverTalk(name: "My Awesome Talk", type: awesomeTalk)
+
+
